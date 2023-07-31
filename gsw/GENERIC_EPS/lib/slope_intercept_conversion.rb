68c6d1dedb4c177ab1c251eb6989598db96832a7
Lucas, John P <John.P.Lucas@nasa.gov>
2023-07-31 08:39:57 -0400

    [nasa/nos3#175] Moved slope intercept conversion from top level COSMOS lib into EPS target for now;

---
+++ gsw/GENERIC_EPS/lib/slope_intercept_conversion.rb
@@ -0,0 +1,21 @@
+require 'openc3/conversions/conversion'
+
+module OpenC3
+  class SlopeInterceptConversion < Conversion
+    def initialize(packet_field, slope, intercept=0)
+      super()
+      @converted_type = :FLOAT
+      @converted_bit_size = 32
+      @packet_field = packet_field
+      @slope = slope.to_f
+      @intercept = intercept.to_f
+    end
+    def call(value, packet, buffer)
+      # Slope
+      value = @slope * packet.read(@packet_field)
+      # Intercept
+      value = value + @intercept
+      return value
+    end
+  end
+end
