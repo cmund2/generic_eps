d861ac0ea12cc0483fde8cd40bd7f7ce54f9324d
Kevin Bruce <kevin.c.bruce@nasa.gov>
2025-04-22 17:58:15 -0400

    [nasa/nos3#580] Updated system tests to have switches turned off in safeing, and to have the data verification necessary. Tested it during day and eclipse

--- gsw/GENERIC_EPS/lib/generic_eps_lib.rb
+++ gsw/GENERIC_EPS/lib/generic_eps_lib.rb
@@ -46,14 +46,72 @@ end
 
 def safe_eps()
     get_eps_hk()
+    sw0_state = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM SWITCH_0_STATE")
+    sw1_state = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM SWITCH_1_STATE")
+    sw2_state = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM SWITCH_2_STATE")
+    sw3_state = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM SWITCH_3_STATE")
+    sw4_state = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM SWITCH_4_STATE")
+    sw5_state = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM SWITCH_5_STATE")
+    sw6_state = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM SWITCH_6_STATE")
+    sw7_state = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM SWITCH_7_STATE")
+
+    if(sw0_state == "ON")
+        eps_cmd("GENERIC_EPS GENERIC_EPS_SWITCH_CC with SWITCH_NUMBER SWITCH_0, STATE OFF")
+    end
+    if(sw1_state == "ON")
+        eps_cmd("GENERIC_EPS GENERIC_EPS_SWITCH_CC with SWITCH_NUMBER SWITCH_1, STATE OFF")
+    end
+    if(sw2_state == "ON")
+        eps_cmd("GENERIC_EPS GENERIC_EPS_SWITCH_CC with SWITCH_NUMBER SWITCH_2, STATE OFF")
+    end
+    if(sw3_state == "ON")
+        eps_cmd("GENERIC_EPS GENERIC_EPS_SWITCH_CC with SWITCH_NUMBER SWITCH_3, STATE OFF")
+    end
+    if(sw4_state == "ON")
+        eps_cmd("GENERIC_EPS GENERIC_EPS_SWITCH_CC with SWITCH_NUMBER SWITCH_4, STATE OFF")
+    end
+    if(sw5_state == "ON")
+        eps_cmd("GENERIC_EPS GENERIC_EPS_SWITCH_CC with SWITCH_NUMBER SWITCH_5, STATE OFF")
+    end
+    if(sw6_state == "ON")
+        eps_cmd("GENERIC_EPS GENERIC_EPS_SWITCH_CC with SWITCH_NUMBER SWITCH_6, STATE OFF")
+    end
+    if(sw7_state == "ON")
+        eps_cmd("GENERIC_EPS GENERIC_EPS_SWITCH_CC with SWITCH_NUMBER SWITCH_7, STATE OFF")
+    end
+
 end
 
 def confirm_eps_data()
     dev_cmd_cnt = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM DEVICE_COUNT")
     dev_cmd_err_cnt = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM DEVICE_ERR_COUNT")
-    
+
+    # Test with Sample Switch Disabled
+    in_sun = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA IN_SUN")
     get_eps_hk()
-    
+    initial_batt_voltage = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM BATT_VOLTAGE")
+
+    eps_cmd("GENERIC_EPS GENERIC_EPS_SWITCH_CC with SWITCH_NUMBER SWITCH_0, STATE OFF")
+    # Should charge during daytime and discharge at night with ambient power draw
+    if(in_sun != 0)
+        wait_check("GENERIC_EPS GENERIC_EPS_HK_TLM BATT_VOLTAGE >= #{initial_batt_voltage}", GENERIC_EPS_RESPONSE_TIMEOUT)
+    else
+        wait_check("GENERIC_EPS GENERIC_EPS_HK_TLM BATT_VOLTAGE <= #{initial_batt_voltage}", GENERIC_EPS_RESPONSE_TIMEOUT)
+    end
+
+    # Test with Sample Switch Enabled
+    eps_cmd("GENERIC_EPS GENERIC_EPS_SWITCH_CC with SWITCH_NUMBER SWITCH_0, STATE ON")
+    in_sun = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA IN_SUN")
+    get_eps_hk()
+    initial_batt_voltage = tlm("GENERIC_EPS GENERIC_EPS_HK_TLM BATT_VOLTAGE")
+
+    #Always draws more power than is being put in if Sample is enabled
+    if(in_sun != 0)
+        wait_check("GENERIC_EPS GENERIC_EPS_HK_TLM BATT_VOLTAGE <= #{initial_batt_voltage}", GENERIC_EPS_RESPONSE_TIMEOUT)
+    else
+        wait_check("GENERIC_EPS GENERIC_EPS_HK_TLM BATT_VOLTAGE <= #{initial_batt_voltage}", GENERIC_EPS_RESPONSE_TIMEOUT)
+    end
+    eps_cmd("GENERIC_EPS GENERIC_EPS_SWITCH_CC with SWITCH_NUMBER SWITCH_0, STATE OFF")
 
     get_eps_hk()
     check("GENERIC_EPS GENERIC_EPS_HK_TLM DEVICE_COUNT >= #{dev_cmd_cnt}")
