7ac87ec42b66ac4a005657c46e2737973a2f805b
Kushal Parekh <kup9@pitt.edu>
2025-06-05 19:13:39 +0000

    [nasa/nos3#726] eps commands and telemetry added

--- fsw/fprime/eps_src/Generic_eps.hpp
+++ fsw/fprime/eps_src/Generic_eps.hpp
@@ -8,6 +8,23 @@
 #define Components_Generic_eps_HPP
 
 #include "eps_src/Generic_epsComponentAc.hpp"
+#include "eps_src/Generic_eps_StateEnumAc.hpp"
+#include "eps_src/Generic_eps_FlagEnumAc.hpp"
+#include "eps_src/Generic_eps_SW_NUMEnumAc.hpp"
+
+extern "C"{
+  #include "generic_eps_device.h"
+  #include "libi2c.h"
+  }
+
+typedef struct
+{
+    uint8_t                     DeviceCount;
+    uint8_t                     DeviceErrorCount;
+    uint8_t                     CommandErrorCount;
+    uint8_t                     CommandCount;
+} __attribute__((packed)) EPS_Hk_tlm_t;
+#define EPS_HK_TLM_LNGTH sizeof(EPS_Hk_tlm_t)
 
 namespace Components {
 
@@ -17,6 +34,9 @@ namespace Components {
 
     public:
 
+      i2c_bus_info_t Generic_epsI2c;
+      GENERIC_EPS_Device_HK_tlm_t Generic_epsHK;
+      EPS_Hk_tlm_t HkTelemetryPkt;
       // ----------------------------------------------------------------------
       // Component construction and destruction
       // ----------------------------------------------------------------------
@@ -35,18 +55,34 @@ namespace Components {
       // Handler implementations for commands
       // ----------------------------------------------------------------------
 
-      //! Handler implementation for command TODO
-      //!
-      //! TODO
-      // void TODO_cmdHandler(
-      //     FwOpcodeType opCode, //!< The opcode
-      //     U32 cmdSeq //!< The command sequence number
-      // ) override;
+      void NOOP_cmdHandler(
+        FwOpcodeType opCode,
+        U32 cmdSeq
+      ) override;
+
+      void RESET_COUNTERS_cmdHandler(
+        FwOpcodeType opCode,
+        U32 cmdSeq
+      ) override;
+
+      void SWITCH_cmdHandler(
+        FwOpcodeType opCode,
+        U32 cmdSeq,
+        Generic_eps_SW_NUM switch_num,
+        Generic_eps_State switch_state
+      ) override;
 
       void REQUEST_HOUSEKEEPING_cmdHandler(
         FwOpcodeType opCode, 
         U32 cmdSeq
       ) override;
+
+      void Update_Counters();
+      void Update_Base_Tlm();
+      void Update_SW_Tlm();
+
+      inline Generic_eps_State get_switch_state(uint16_t switch_status);
+      inline Generic_eps_Flag get_switch_flag(uint16_t switch_status);
 
   };
 
